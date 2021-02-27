bab9380f7c9917959f2131f43d4bc0c48136d67b
Joe Wu <wuzongsheng@gmail.com>
2021-02-27 18:49:51 +0800

    add price

---
+++ unittest/ut_price.cpp
@@ -0,0 +1,125 @@
+#include <gtest/gtest.h>
+
+#include <cmath>    // std::fabs
+
+#include "ref/price.hpp"
+
+using miu::ref::price;
+
+TEST(ut_price, value) {
+    EXPECT_DOUBLE_EQ(0.0, static_cast<double>(price {}));    // default value
+
+    EXPECT_DOUBLE_EQ(1.234, static_cast<double>(price { 1.234 }));
+    EXPECT_DOUBLE_EQ(0.000005, static_cast<double>(price { 0.000005 }));
+}
+
+TEST(ut_price, equility) {
+    EXPECT_TRUE(price { 1.2 } == price { 1.2 });
+    EXPECT_TRUE(price { 1.2 } == price { 1.2 + price::epsilon() });
+    EXPECT_TRUE(price { 1.2 } == price { 1.2 - price::epsilon() });
+
+    EXPECT_FALSE(price { 1.2 } == price { 1.1 });
+}
+
+TEST(ut_price, greater) {
+    EXPECT_TRUE(price { 1.2 } > price { 1.1 });
+    EXPECT_TRUE(price { 1.2 } > 1.1);
+
+    EXPECT_FALSE(price(1.2) > price { 1.3 });
+    EXPECT_FALSE(price { 1.2 + price::epsilon() } > price { 1.2 });
+    EXPECT_FALSE(price { 1.2 } > price { 1.2 - price::epsilon() });
+}
+
+TEST(ut_price, greater_equal) {
+    EXPECT_TRUE(price { 1.2 } >= price { 1.1 });
+    EXPECT_TRUE(price { 1.2 } >= 1.1);
+    EXPECT_TRUE(price { 1.2 } >= price { 1.2 });
+    EXPECT_TRUE(price { 1.2 } >= price { 1.2 + price::epsilon() });
+    EXPECT_TRUE(price { 1.2 - price::epsilon() } >= price { 1.2 });
+
+    EXPECT_FALSE(price { 1.2 } >= price { 1.3 });
+}
+
+TEST(ut_price, less) {
+    EXPECT_TRUE(price { 1.2 } < price { 1.3 });
+    EXPECT_TRUE(price { 1.2 } < 1.3);
+
+    EXPECT_FALSE(price(1.2) < price { 1.1 });
+    EXPECT_FALSE(price { 1.2 - price::epsilon() } < price { 1.2 });
+    EXPECT_FALSE(price { 1.2 } < price { 1.2 + price::epsilon() });
+}
+
+TEST(ut_price, less_equal) {
+    EXPECT_TRUE(price { 1.2 } <= price { 1.3 });
+    EXPECT_TRUE(price { 1.2 } <= 1.3);
+    EXPECT_TRUE(price { 1.2 } <= price { 1.2 });
+    EXPECT_TRUE(price { 1.2 } <= price { 1.2 + price::epsilon() });
+    EXPECT_TRUE(price { 1.2 - price::epsilon() } <= price { 1.2 });
+
+    EXPECT_FALSE(price { 1.2 } <= price { 1.1 });
+}
+
+TEST(ut_price, addition) {
+    EXPECT_EQ(price { 0.8 }, price { 0.6 } + price { 0.2 });
+    EXPECT_EQ(price { 0.8 }, price { 0.6 } + 0.2);
+    EXPECT_EQ(price { 0.8 }, 0.6 + price { 0.2 });
+
+    auto prc = price { 0.8 };
+    EXPECT_EQ(price { 0.9 }, prc += price { 0.1 });
+    EXPECT_EQ(price { 0.9 }, prc);
+    EXPECT_EQ(price { 1.1 }, prc += 0.2);
+    EXPECT_EQ(price { 1.1 }, prc);
+
+    double val = 0.1;
+    val += prc;
+    EXPECT_DOUBLE_EQ(1.2, val);
+}
+
+TEST(ut_price, subtraction) {
+    EXPECT_EQ(price { 0.8 }, price { 1.2 } - price { 0.4 });
+    EXPECT_EQ(price { 0.8 }, price { 1.2 } - 0.4);
+    EXPECT_EQ(price { 0.8 }, 1.2 - price { 0.4 });
+
+    auto prc = price { 0.8 };
+    EXPECT_EQ(price { 0.7 }, prc -= price { 0.1 });
+    EXPECT_EQ(price { 0.7 }, prc);
+    EXPECT_EQ(price { 0.5 }, prc -= 0.2);
+    EXPECT_EQ(price { 0.5 }, prc);
+
+    double val = 0.1;
+    val -= prc;
+    EXPECT_DOUBLE_EQ(-0.4, val);
+}
+
+TEST(ut_price, multiplication) {
+    EXPECT_EQ(price { 0.8 }, price { 0.2 } * price { 4 });
+    EXPECT_EQ(price { 0.8 }, price { 0.2 } * 4);
+    EXPECT_EQ(price { 0.8 }, 0.2 * price { 4 });
+
+    auto prc = price { 0.8 };
+    EXPECT_EQ(price { 1.6 }, prc *= price { 2 });
+    EXPECT_EQ(price { 1.6 }, prc);
+    EXPECT_EQ(price { 0.32 }, prc *= 0.2);
+    EXPECT_EQ(price { 0.32 }, prc);
+
+    double val = 0.1;
+    val *= prc;
+    EXPECT_DOUBLE_EQ(0.032, val);
+}
+
+TEST(ut_price, division) {
+    EXPECT_EQ(price { 0.8 }, price { 1.6 } / price { 2 });
+    EXPECT_EQ(price { 0.8 }, price { 1.6 } / 2);
+    EXPECT_EQ(price { 0.8 }, 1.6 / price { 2 });
+
+    auto prc = price { 0.8 };
+    EXPECT_EQ(price { 0.4 }, prc /= price { 2 });
+    EXPECT_EQ(price { 0.4 }, prc);
+    EXPECT_EQ(price { 2 }, prc /= 0.2);
+    EXPECT_EQ(price { 2 }, prc);
+
+    double val = 0.1;
+    val /= prc;
+    EXPECT_DOUBLE_EQ(0.05, val);
+}
+
